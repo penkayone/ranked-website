b8987c418509939a5bf0dd333a38ee206c83d129
penkayone <An.goloskokov@gmail.com>
2025-04-01 18:48:37 +0300

    I made box-shadow for header and changed the color (as in figma), fixed bugs that interfered with the work of the site, finalized the main page, and more precisely made the buttons in the center, because there were problems because of bugs on the site, and a couple of other little things. Made the font for the whole site Inter Regular, made @media - site for phone, now the site for tablets and phones. Aligned the menu for media and made a clickable icon where the buttons are. Compressed the background (it is necessary for that, if the user comes from the phone, it was faster to load the background of the site.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+console.log('Happy developing ✨');
+
+/*
+Burger menu
+ */
+$('.burger-menu').click(function () {
+    $('.nav-buttons').toggleClass('mobile');
+    $(this).html('<i class="fa-solid fa-xmark"></i>');
+});
