5995b8ec61087c0e1ab8573fd8932f18ef79c072
penkayone <An.goloskokov@gmail.com>
2025-04-05 14:30:08 +0300

    Added an Admin panel in Updates to create posts automatically, but needs to be finalized.

--- assets/js/updates.js
+++ assets/js/updates.js
@@ -32,3 +32,36 @@ function resetFilters() {
     console.log('Filters reset');
     // В реальном проекте здесь будет код для отображения всего контента
 }
+
+document.getElementById('admin-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const title = document.getElementById('post-title').value;
+    const author = document.getElementById('post-author').value;
+    const date = document.getElementById('post-date').value;
+    const image = document.getElementById('post-image').value;
+    const content = document.getElementById('post-content').value;
+
+    const newPostHTML = `
+    <div class="update-card">
+        <img src="${image}" alt="">
+        <div class="content">
+            <h2 class="update-title gradient-text">${title}</h2>
+            <div class="update-meta">
+                <span class="update-date"><i class="fa-regular fa-calendar"></i> ${date}</span>
+                <span class="update-author"><i class="fa-regular fa-user"></i> ${author}</span>
+                <span class="update-version"><i class="fa-solid fa-eye"></i> 0 views</span>
+            </div>
+            <p class="update-content">${content}</p>
+        </div>
+        <a href="#" class="click-read">
+            <i class="fas fa-arrow-right"></i>
+            <span>Click to Read!</span>
+        </a>
+    </div>`;
+
+    document.querySelector('.content-area').insertAdjacentHTML('afterbegin', newPostHTML);
+
+    // Очистка формы
+    this.reset();
+});
